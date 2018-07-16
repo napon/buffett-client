4c1b238fce52d9506298e7070a686790baf485b0
Napon Taratan <napon.taratan@gmail.com>
2018-07-15 19:47:51 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Stock 101</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <Dashboard />
+    </v-content>
+    <v-footer app fixed></v-footer>
+  </v-app>
+</template>
+
+<script>
+  import Dashboard from './components/Dashboard'
+  export default {
+    components: {
+        Dashboard
+    },
+    data: () => ({
+      drawer: false
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  margin-top: 5vh;
+}
+</style>
