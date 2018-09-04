531a2d5af4f1d9b4523648fc22d20ee2d65bf2b4
Napon Taratan <napon.taratan@gmail.com>
2018-09-03 22:56:58 -0700

    Fix up heroku config file linting errors

--- heroku.js
+++ heroku.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const serveStatic = require("serve-static")
+/* eslint-env node */
+const express = require('express'); // eslint-disable-line import/no-extraneous-dependencies
+const serveStatic = require('serve-static'); // eslint-disable-line import/no-extraneous-dependencies
 const path = require('path');
-app = express();
+
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
